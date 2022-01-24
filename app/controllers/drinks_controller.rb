8339d7c914d21b92de71aae6522d4b0d560f232d
Luke Hooper <Laaronhooper@gmail.com>
2022-01-24 22:11:51 +1100

    drink api/controller

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,65 +1,65 @@
-class AlcoholsController < ApplicationController
+class DrinksController < ApplicationController
     before_action :authenticate_user, except: [:index]
-    before_action :set_alcohol, only: [:show,:update,:destroy]
+    before_action :set_drink, only: [:show,:update,:destroy]
     before_action :check_ownership, only: [:update,:destroy]
 
-    # Get all Alcohols
-    def index
-        @alcohols = Alcohol.all
+    # Get all Drinks
+    # def index
+    #     @drinks = Drink.all
         
-        if @alcohols.length > 1
-            render json: @alcohols
-        else
-            render json: {error: "No alcohol in database"}, status: 404
-        end
-    end
+    #     if @drinks.length > 1
+    #         render json: @drinks
+    #     else
+    #         render json: {error: "No drink in database"}, status: 404
+    #     end
+    # end
 
-    def create
-        @alcohol = current_user.alcohols.create(alcohol_params)
+    # def create
+    #     @drink = current_user.drinks.create(drink_params)
 
-        if @alcohol.errors.any?
-            render json: @alcohol.errors, status: :unprocessable_entity
-        else
-            render json: @alcohol, status: 201
-        end
-    end
+    #     if @drink.errors.any?
+    #         render json: @drink.errors, status: :unprocessable_entity
+    #     else
+    #         render json: @drink, status: 201
+    #     end
+    # end
 
-    def update
-        @alcohol = Alcohol.find_by_id(params[:id])
+    # def update
+    #     @drink = Drink.find_by_id(params[:id])
 
-        @alcohol.update(alcohol_params)
+    #     @drink.update(drink_params)
 
-        if @alcohol.errors.any?
-            render json: @alcohol.errors, status: :unprocessable_entity
-        else
-            render json: @alcohol, status: 200
-        end
+    #     if @drink.errors.any?
+    #         render json: @drink.errors, status: :unprocessable_entity
+    #     else
+    #         render json: @drink, status: 200
+    #     end
 
-    end
+    # end
 
-    private
-     def alcohol_params
-        params.require(:alcohol).permit(:name,:volume_in_ml,:critical_volume,:user_id)
-     end
+    # private
+    #  def drink_params
+    #     params.require(:drink).permit(:name,:volume_in_ml,:critical_volume,:user_id)
+    #  end
 
-     def set_alcohol
-        begin
-            @alcohol=Alcohol.find(params[:id])
-        rescue
-            render json: {error: "Alcohol not found"}, status: 404
+    #  def set_drink
+    #     begin
+    #         @drink=Drink.find(params[:id])
+    #     rescue
+    #         render json: {error: "Drink not found"}, status: 404
             
-        end
+    #     end
 
-     end
+    #  end
 
 
-     # This is the logic to check if current user is the alcohol owner OR if the current user is an admin
-     def check_ownership
-        if current_user.id === @alcohol.user_id || current_user.admin
-            return
-        else
-            render json: {error: "You don't have permission to do that"}, status: 401
-        end
-     end
+    #  # This is the logic to check if current user is the drink owner OR if the current user is an admin
+    #  def check_ownership
+    #     if current_user.id === @drink.user_id || current_user.admin
+    #         return
+    #     else
+    #         render json: {error: "You don't have permission to do that"}, status: 401
+    #     end
+    #  end
 
 end
