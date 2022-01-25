b078dd302e7e6033ee7ba958ffc0f35a896ee7e5
Si Yang Cui <siyangcui@gmail.com>
2022-01-25 12:47:10 +1100

    added model relationship on user to drinks and user added model level validation and uniqueness

--- app/models/mixer.rb
+++ app/models/mixer.rb
@@ -1,2 +1,7 @@
 class Mixer < ApplicationRecord
+  belongs_to :user
+  has_many :drinks, dependent: :destroy
+
+  validates :name, :volume_in_ml, :critical_volume, presence: true
+  validates :name, uniqueness: true
 end
